618f2a6847e1ced1fada25ceb2ea69ba2c293bb6
Jessyca430 <moag6k@yahoo.fr>
2021-06-18 17:59:41 +0200

    mmm

--- 03-multiples-of-number/multiples_of_number.js
+++ 03-multiples-of-number/multiples_of_number.js
@@ -1,1 +1,13 @@
 // define your function here
+const multiplesOf = (number, limit) => {
+    const multiples = [];
+    for (let i=0; i<limit; i++){
+        if (i%number === 0) {
+            multiples.push(i);
+        }  
+    }
+    return multiples
+}
+constmultiplesofTwo = multiplesOf(2,20);
+console.log(multiplesTwo);
+
