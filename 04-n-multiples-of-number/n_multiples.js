9d98ffe97b800d21cdba90289a2715c879afc097
Jessyca Laudet <moag6k@yahoo.fr>
2021-06-19 10:23:07 +0200

    Fait

--- 04-n-multiples-of-number/n_multiples.js
+++ 04-n-multiples-of-number/n_multiples.js
@@ -1,6 +1,11 @@
 // function declaration
-const nMultiplesOf = (number, limit) => {
-
+function multiplesOf(number){
+    let multiples = number[0];
+    for (let i = 0; i< number.length; i++){
+        if(number[i] % multiples === 0){
+            multiples = number [i];
+        }
+    }
+    return multiples;
 }
-
-console.log(nMultiplesOf(2, 10))
+console.log(multiplesOf([2, 4, 6, 8, 10, 12, 14, 16, 18, 20],  2));
