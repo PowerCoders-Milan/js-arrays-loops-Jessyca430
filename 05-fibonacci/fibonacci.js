faa92ff67e3bfc9311d6769222d97bc08a02dbc2
Jessyca Laudet <moag6k@yahoo.fr>
2021-06-20 15:08:46 +0200

    a revoir

--- 05-fibonacci/fibonacci.js
+++ 05-fibonacci/fibonacci.js
@@ -2,3 +2,11 @@
 const maxNumber = Math.ceil(Math.random()*200);
 let n1 = 0, n2 = 1, nextTerm;
 
+console.log('fibonacci series:') ;
+for (let i= 1; i <= Number; i++) {
+    console.log(n1);
+    n1 = n2;
+    n2 = nextTerm;
+}    
+getFibonacciSequence(maxNumber)
+maxNumber 
